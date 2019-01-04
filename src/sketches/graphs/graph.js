b842e5b41842890247bd7cbee178c97e5e79d13b
Jakub Pawlica <jakub.pawlica@gmail.com>
2019-01-04 23:28:03 +0100

    Added a way to create adjacency matrix

--- src/sketches/graphs/graph.js
+++ src/sketches/graphs/graph.js
@@ -2,12 +2,30 @@ class Graph {
   constructor(nodes) {
     this.nodes = [...nodes];
     this.edges = [];
+    this.adjacencyMatrix = [];
   }
 
   generateEdges = () => {
-    this.nodes.forEach(node =>
-      node.neighbors.forEach(neighbor => this.edges.push({ source: node.id, target: neighbor }))
-    );
+    for (const node of this.nodes) {
+      for (const neighbor of node.neighbors) {
+        this.edges.push({ source: node.id, target: neighbor });
+      }
+    }
+  };
+
+  createAdjacencyMatrix = () => {
+    for (const node of this.nodes) {
+      // Connections of the current node
+      const connections = [];
+      // Check all nodes
+      for (const target of this.nodes) {
+        // Check given connection is actually an edge in the graph
+        this.edges.some(edge => edge.source === node.id && edge.target === target.id)
+          ? connections.push(1)
+          : connections.push(0);
+      }
+      this.adjacencyMatrix.push(connections);
+    }
   };
 }
 
