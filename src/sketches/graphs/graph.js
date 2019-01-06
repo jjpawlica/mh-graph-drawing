10bd23c69990c35fa0503ad2951142b966dfbb56
Jakub Pawlica <jakub.pawlica@gmail.com>
2019-01-06 19:55:55 +0100

    Graph helper functions fixes

--- src/sketches/graphs/graph.js
+++ src/sketches/graphs/graph.js
@@ -8,7 +8,9 @@ class Graph {
   generateEdges = () => {
     for (const node of this.nodes) {
       for (const neighbor of node.neighbors) {
-        this.edges.push({ source: node.id, target: neighbor });
+        if (!this.edges.some(edge => edge.source === neighbor && edge.target === node.id)) {
+          this.edges.push({ source: node.id, target: neighbor });
+        }
       }
     }
   };
@@ -20,7 +22,11 @@ class Graph {
       // Check all nodes
       for (const target of this.nodes) {
         // Check given connection is actually an edge in the graph
-        this.edges.some(edge => edge.source === node.id && edge.target === target.id)
+        this.edges.some(
+          edge =>
+            (edge.source === node.id && edge.target === target.id) ||
+            (edge.source === target.id && edge.target === node.id)
+        )
           ? connections.push(1)
           : connections.push(0);
       }
