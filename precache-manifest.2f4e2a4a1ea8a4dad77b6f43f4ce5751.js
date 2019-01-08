992080639a11c351786b41c7bc6492f647056a2c
jjpawlica <jakub.pawlica@gmail.com>
2019-01-08 21:13:54 +0100

    Updates

---
+++ precache-manifest.2f4e2a4a1ea8a4dad77b6f43f4ce5751.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "12e5c48afe4357a98491",
+    "url": "/mh-graph-drawing/static/css/main.d278dce6.chunk.css"
+  },
+  {
+    "revision": "12e5c48afe4357a98491",
+    "url": "/mh-graph-drawing/static/js/main.12e5c48a.chunk.js"
+  },
+  {
+    "revision": "90aadf2e3e06a8d12c01",
+    "url": "/mh-graph-drawing/static/js/1.90aadf2e.chunk.js"
+  },
+  {
+    "revision": "b23b2d38a456cd2b13d1",
+    "url": "/mh-graph-drawing/static/js/runtime~main.b23b2d38.js"
+  },
+  {
+    "revision": "a8204f6598868050a00ebf76fefa62fd",
+    "url": "/mh-graph-drawing/index.html"
+  }
+];
